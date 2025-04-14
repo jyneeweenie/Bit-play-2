bcc2548e077ce1d081e89988e4fc6d62561bc357
jyneeweenie <jynemarie1@gmail.com>
2025-04-14 11:46:35 +0300

    done

--- Assignment/main.py
+++ Assignment/main.py
@@ -1,1 +1,12 @@
-\n
+num = int(input("Enter a number: "))
+
+# Get the binary representation (remove '0b' prefix)
+binary = bin(num)[2:]
+
+# Reverse the binary string
+reversed_binary = binary[::-1]
+
+# Convert back to integer
+reversed_num = int(reversed_binary, 2)
+
+print(f"Reversed bits number: {reversed_num}")
